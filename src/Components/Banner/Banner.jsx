8f55d67bc142f2a92ca85d892d58ac3ff16ab414
muntasir mamun <mdmuntasirmamun63@gmail.com>
2023-12-29 18:28:25 +0600

    navbar and banner section added done

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="container mx-auto">
+                <div>
+                    <div>
+                        <div className='flex flex-col-reverse md:flex-row md:justify-between justify-center gap-10 items-center my-24'>
+                            <div className='md:w-1/2 w-full text-center md:text-start'>
+                                <h1 className="text-7xl text-white font-bold">Md Muntasir <span className='text-secondary'> Mamun</span></h1>
+                                <p className='text-3xl font-bold mt-5'><span className='text-secondary text-4xl'>Mern </span> stack Junior Web Developer</p>
+                                <button className='group relative h-12 w-48 bg-white overflow-hidden rounded-lg text-lg font-bold shadow mt-5 px-1'><a href="">
+                                    <div className='absolute inset-0 w-1 bg-secondary transition-all duration-[250ms] ease-out group-hover:w-full'> </div>
+                                    <span className='relative text-primary group-hover:text-white'>See Resume</span>
+                                </a>
+                                </button>
+                            </div>
+                            <div className='md:w-96 md:h-96 w-full relative h-full rounded'>
+                                <img src="https://i.ibb.co/1q4LMpQ/Whats-App-Image-2023-12-29-at-18-18-35-8757989a-removebg-preview.png" alt="MD Muntasir Mamun" className='rounded-full w-full h-full' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-none md:px-32 py-6 items-end">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                      
+                        <label className="btn btn-ghost lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg></label>
+                        <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary border border-secondary rounded-box w-52 py-10"><li className="hover:text-secondary mx-auto"><a aria-current="page" className="active" href="/"><span className="text-2xl font-bold">Home</span></a></li><li className="hover:text-secondary mx-auto"><a className="" href="/about"><span className="text-2xl font-bold">About</span></a></li><li className="hover:text-secondary mx-auto"><a class="" href="/skills"><span className="text-2xl font-bold">Skills</span></a></li><li className="hover:text-secondary mx-auto"><a class="" href="/services"><span className="text-2xl font-bold">Services</span></a></li><li className="hover:text-secondary mx-auto"><a class="" href="/projects"><span className="text-2xl font-bold">Projects</span></a></li><li className="hover:text-secondary mx-auto"><a class="" href="/contact"><span className="text-2xl font-bold">Contact</span></a></li><div className="flex justify-center items-center"></div></ul>
+                    </div>
+                    <a className="cursor-pointer text-xl flex items-end" href="/">
+                        <img className='w-16' src="https://i.ibb.co/jHbyW2c/p.png" alt="" />
+                        <span className="text-5xl font-bold">ort</span><span class="text-5xl font-bold text-secondary">folio</span></a>
+                </div>
+                <div className="navbar-center hidden lg:flex items-center ">
+                    <ul className='menu menu-horizontal px-1 items-center'>
+                      <li className='hover:text-secondary px-1'>
+                        <a className='active ' href="">Home</a>
+                      </li>
+                      <li className='hover:text-secondary px-1'>
+                        <a className='active' href="">About</a>
+                      </li>
+                      <li className='hover:text-secondary px-1'>
+                        <a className='active' href="">Skill</a>
+                      </li>
+                      <li className='hover:text-secondary px-1'>
+                        <a className='active' href="">Services</a>
+                      </li>
+                      <li className='hover:text-secondary px-1'>
+                        <a className='active' href="">Projects</a>
+                      </li>
+                      <li className='hover:text-secondary px-1'>
+                        <a className='active' href="">Contact</a>
+                      </li>
+                      <span class="mx-5 text-secondary">||</span>
+                      <li className="rounded-full hover:text-secondary"><a className="rounded-full" href="https://github.com/JaznanOfficial" target="_blank" rel="noreferrer"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 496 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg></a></li>
+                      <li class="rounded-full hover:text-secondary"><a className="rounded-full" href="https://www.linkedin.com/in/jaznanofficial/" target="_blank" rel="noreferrer"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg></a></li>
+                      <li className="rounded-full hover:text-secondary"><a className="rounded-full" href="https://www.facebook.com/jaznan.official/" target="_blank" rel="noreferrer"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg></a></li>
+                      
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
+
+
+
+{/* <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary border border-secondary rounded-box w-52 py-10"><li class="hover:text-secondary mx-auto"><a aria-current="page" class="active" href="/" style="color: rgb(0, 192, 255);"><span class="text-2xl font-bold">Home</span></a></li><li class="hover:text-secondary mx-auto"><a class="" href="/about"><span class="text-2xl font-bold">About</span></a></li><li class="hover:text-secondary mx-auto"><a class="" href="/skills"><span class="text-2xl font-bold">Skills</span></a></li><li class="hover:text-secondary mx-auto"><a class="" href="/services"><span class="text-2xl font-bold">Services</span></a></li><li class="hover:text-secondary mx-auto"><a class="" href="/projects"><span class="text-2xl font-bold">Projects</span></a></li><li class="hover:text-secondary mx-auto"><a class="" href="/contact"><span class="text-2xl font-bold">Contact</span></a></li><span class="my-5 text-secondary mx-auto font-bold text-3xl"><svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><title></title><path d="M.95 14.184L12 20.403l9.919-5.55v2.21L12 22.662l-10.484-5.96-.565.308v.77L12 24l11.05-6.218v-4.317l-.515-.309L12 19.118l-9.867-5.653v-2.21L12 16.805l11.05-6.218V6.32l-.515-.308L12 11.974 2.647 6.681 12 1.388l7.76 4.368.668-.411v-.566L12 0 .95 6.27v.72L12 13.207l9.919-5.55v2.26L12 15.52 1.516 9.56l-.565.308Z"></path></svg></span><div class="flex justify-center items-center"><li class="rounded-full hover:text-secondary "><a class="rounded-full font-bold text-3xl" href="https://github.com/MuntasirMamuon" target="_blank" rel="noreferrer"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 496 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg></a></li><li class="rounded-full hover:text-secondary"><a class="rounded-full font-bold text-3xl" href="https://www.linkedin.com/in/mohammad-muntasir-mamun-8592a4292/" target="_blank" rel="noreferrer"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg></a></li><li class="rounded-full hover:text-secondary"><a class="rounded-full font-bold text-3xl" href="https://www.facebook.com/mdmuntasir.mamun.589" target="_blank" rel="noreferrer"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg></a></li></div></ul> */}
